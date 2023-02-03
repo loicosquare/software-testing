355f9ee25a20cbdae3f4d0b51e5401d458377082
loicosquare <loicsan07@gmail.com>
2023-02-03 16:52:59 +0100

    MockStripeService

--- src/main/java/com/fstl/testing/stripe/MockStripeService.java
+++ src/main/java/com/fstl/testing/stripe/MockStripeService.java
@@ -3,13 +3,23 @@ package com.fstl.testing.stripe;
 import com.fstl.testing.payment.CardPaymentCharge;
 import com.fstl.testing.payment.CardPaymentCharger;
 import com.fstl.testing.payment.Currency;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
+@Service
+@ConditionalOnProperty( //String w'll use this property if the stripe.enabled having false value [see application.properties]
+        value = "stripe.enabled",
+        havingValue = "false"
+)
 public class MockStripeService implements CardPaymentCharger {
 
     @Override
-    public CardPaymentCharge chargeCard(String source, BigDecimal amount, Currency currency, String description) {
-        return null;
+    public CardPaymentCharge chargeCard(String cardSource,
+                                        BigDecimal amount,
+                                        Currency currency,
+                                        String description) {
+        return new CardPaymentCharge(true);
     }
 }
